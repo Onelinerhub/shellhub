92310bc8ad217d0d419f4ae35fe0c13ddfce5cc1
Joe Markberg / code editor <inonunicorn@gmail.com>
2022-01-20 15:06:12 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,3 @@
 sudo apt install -y xdotool
 sudo curl https://raw.githubusercontent.com/Onelinerhub/shellhub/main/oh.sh -o /usr/local/bin/oh
-sudo chmox +x /usr/local/bin/oh
+sudo chmod +x /usr/local/bin/oh
